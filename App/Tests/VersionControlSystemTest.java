33aa9e834775cd5113111daf64c24023fa31ea70
Chengming Li <chengming.li@berkeley.edu>
2023-03-29 16:40:24 -0700

    Rewrote instantiation, implemented half of add()

--- App/Tests/VersionControlSystemTest.java
+++ App/Tests/VersionControlSystemTest.java
@@ -1,34 +1,52 @@
 import static org.junit.jupiter.api.Assertions.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.Objects;
 
 class VersionControlSystemTest {
-    private static final String testDir = "C:/Users/malic/Downloads/Project/VersionControlSystem/Test";
+    private static final String testDir = "C:\\Users\\malic\\Downloads\\Project\\VersionControlSystem\\Test";
     @org.junit.jupiter.api.Test
     void init() {
         VersionControlSystem vcs = cleanUp();
-        assertTrue(Files.exists(Paths.get(testDir + "/.vcs")));
+        assertTrue(Files.exists(Paths.get(testDir + "\\.vcs")));
     }
     @org.junit.jupiter.api.Test
     void testCreateFile() {
         VersionControlSystem vcs = cleanUp();
         try {
-            FileWriter writer = new FileWriter(testDir +"/testText.txt");
+            FileWriter writer = new FileWriter(testDir +"\\testText.txt");
             writer.write("This is some nice text, yada yada ");
             writer.close();
-            String hash = vcs.hash(testDir +"/testText.txt");
+            String hash = vcs.hash(testDir +"\\testText.txt");
             assertFalse(vcs.hashExists(hash));
-            assertTrue(vcs.createFile(testDir +"/testText.txt", hash));
+            assertTrue(vcs.createFile(testDir +"\\testText.txt", hash));
             assertTrue(vcs.hashExists(hash));
         } catch (Exception e) {
             e.printStackTrace();
-            assertTrue(false);
+            fail();
+        }
+    }
+    @org.junit.jupiter.api.Test
+    void addTest() {
+        VersionControlSystem vcs = cleanUp();
+        try {
+            FileWriter writer = new FileWriter(testDir +"\\testText.txt");
+            writer.write("This is some nice text, yada yada");
+            writer.close();
+            vcs.add(testDir +"\\testText.txt", 0);
+            BufferedReader br = new BufferedReader(new FileReader(testDir +"\\.vcs\\Index"));
+            String firstLine = String.format("%s %s 0", "testText.txt", vcs.hash(testDir +"\\testText.txt"));
+            assertEquals(br.readLine(), firstLine);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
         }
     }
     private static VersionControlSystem cleanUp() {
         File[] files = new File(testDir).listFiles();
+        assert files != null;
         for (File file : files) {
             if (file.isDirectory()) {
                 deleteDirectory(file);
@@ -36,9 +54,7 @@ class VersionControlSystemTest {
                 file.delete();
             }
         }
-        VersionControlSystem vcs = new VersionControlSystem();
-        vcs.updateDirectory(testDir);
-        vcs.init();
+        VersionControlSystem vcs = VersionControlSystem.init(testDir);
         return vcs;
     }
     public static void deleteDirectory(File directory) {
--- App/src/VersionControlSystem.java
+++ App/src/VersionControlSystem.java
@@ -1,87 +1,127 @@
-import java.io.File;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
-import java.io.FileInputStream;
 import java.security.MessageDigest;
 import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 
 public class VersionControlSystem {
-    private String currentDirectory;
-    private String vcsDirectory;
+    private final String currentDirectory;
+    private final String vcsDirectory;
+    private final String head;
+    private final String index;
+    private final String objects;
     private final String separator;
-    private final String[] subDirectories = {"Objects"};
-    private final String[] files = {"HEAD", "Index"};
-    public VersionControlSystem() {
-        currentDirectory = System.getProperty("user.dir");
-        vcsDirectory = null;
-        separator = System.getProperty("file.separator");
+    private static final String[] subDirectories = {"Objects"};
+    private static final String[] files = {"HEAD", "Index"};
+    public VersionControlSystem(String currentDirectory) {
+        this.currentDirectory = currentDirectory;
+        this.separator = System.getProperty("file.separator");
+        this.vcsDirectory = pathBuilder(new String[]{".vcs"}, currentDirectory, separator);
+        this.head = pathBuilder(new String[]{"HEAD"}, vcsDirectory, separator);
+        this.index = pathBuilder(new String[]{"Index"}, vcsDirectory, separator);
+        this.objects = pathBuilder(new String[]{"Objects"}, vcsDirectory, separator);
     }
-    /**
-     * Switches directories, updates currentDirectory and vcsDirectory if exists
-     * @param dir: path to the new directory
-     */
-    public void updateDirectory(String dir) {
-        if (Files.exists(Paths.get(dir))) {
-            this.currentDirectory = dir;
-            File vcs = new File(dir + separator + ".vcs");
-            if (vcs.exists() && vcs.isDirectory()) {
-                this.vcsDirectory = dir + separator + ".vcs";
-            } else {
-                this.vcsDirectory = null;
-            }
-        } else {
-            this.currentDirectory = null;
-            this.vcsDirectory = null;
-        }
+    public VersionControlSystem(String currentDirectory, String vcsDirectory, String head, String index, String objects) {
+        this.currentDirectory = currentDirectory;
+        this.vcsDirectory = vcsDirectory;
+        this.head = head;
+        this.index = index;
+        this.objects = objects;
+        this.separator = System.getProperty("file.separator");
     }
-
     /**
      * The init command, creates a .vcs folder and subfolders to initialize version control system.
      * If a .vcs folder already exists, do nothing
      */
-    public void init() {
-        if (this.currentDirectory != null && Files.exists(Paths.get(currentDirectory))) {
-            File vcs = new File(pathBuilder(new String[] {".vcs"}));
-            if (vcs.exists()) {
-                System.out.println("Version control system already exists");
-                return;
-            }
+    public static VersionControlSystem init(String dir) {
+        String separator = System.getProperty("file.separator");
+        File vcs = new File(pathBuilder(new String[] {".vcs"}, dir, separator));
+        if (!Files.exists(Paths.get(dir))) {
+            System.out.println("Directory doesn't exist");
+        } else if (vcs.exists()) {
+            System.out.println("Version Control System already exists");
+        } else {
             if (vcs.mkdir()) {
+                Map<String, String> sub = new HashMap<>();
                 Path path = vcs.toPath();
                 try {
                     Files.setAttribute(path, "dos:hidden", true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                String vcsDirectory = path.toString();
                 for (String subDirectory : subDirectories) {
-                    File subfolder = new File(pathBuilder(new String[] {".vcs"}), subDirectory);
+                    File subfolder = new File(vcsDirectory, subDirectory);
                     if (!subfolder.mkdir()) {
                         vcs.delete();
-                        System.out.println("Failed to create " + pathBuilder(new String[] {".vcs", subDirectory}));
-                        return;
+                        System.out.println("Failed to create " + pathBuilder(new String[] {subDirectory}, vcsDirectory, separator));
+                        return null;
+                    } else {
+                        sub.put(subDirectory, subfolder.toPath().toAbsolutePath().toString());
                     }
                 }
                 for (String f : files) {
-                    File file = new File(pathBuilder(new String[] {".vcs"}), f);
+                    File file = new File(vcsDirectory, f);
                     try {
                         if (!file.createNewFile()) {
                             vcs.delete();
-                            System.out.println("Failed to create " + pathBuilder(new String[] {".vcs", subDirectories[1], f}));
-                            return;
+                            System.out.println("Failed to create " + pathBuilder(new String[] {f}, vcsDirectory, separator));
+                            return null;
+                        } else {
+                            sub.put(f, file.toPath().toAbsolutePath().toString());
                         }
                     } catch (Exception e) {
                         vcs.delete();
-                        System.out.println("Failed to create " + pathBuilder(new String[] {".vcs", subDirectories[1], f}));
+                        System.out.println("Failed to create " + pathBuilder(new String[] {f}, vcsDirectory, separator));
                         System.out.println(e.getMessage());
-                        return;
+                        return null;
                     }
                 }
+                return new VersionControlSystem(dir, vcsDirectory, sub.get("HEAD"), sub.get("Index"), sub.get("Objects"));
             } else {
-                System.out.println("Failed to create " + pathBuilder(new String[] {".vcs"}));
+                System.out.println("Unable to create " + vcs.toPath());
             }
-        } else {
-            System.out.println("Current directory not set");
+        }
+        return null;
+    }
+    public void add(String path, int status) {
+        try {
+            File head = new File(this.head);
+            File index = new File(this.index);
+            File file = new File(path);
+            String name = file.getName();
+            String hash = hash(path);
+            BufferedReader br = new BufferedReader(new FileReader(head));
+            StringBuilder sb = new StringBuilder();
+            boolean exists = false;
+            String line;
+            if (br.readLine() != null) {
+                return;
+            }
+            br = new BufferedReader(new FileReader(index));
+            while ((line = br.readLine()) != null) {
+                if (line.length() == name.length() + 43 && line.startsWith(name)) {  // space between name and hash, 40 character hash, space between hash and status, and 1 number status
+                    sb.append(String.format("%s %s %d", name, hash, status));
+                    exists = true;
+                } else {
+                    sb.append(line);
+                }
+                sb.append("\n");
+            }
+            if (!exists) {
+                sb.append(String.format("%s %s %d", name, hash, status));
+            }
+            createFile(path, hash);
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(index))) {
+                bw.write(sb.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     /**
@@ -89,9 +129,9 @@ public class VersionControlSystem {
      * @param paths: how to get to the desired destination, in the form of an array of strings
      * @return the desired path
      */
-    private String pathBuilder(String[] paths) {
+    private static String pathBuilder(String[] paths, String start, String separator) {
         StringBuilder output = new StringBuilder();
-        output.append(currentDirectory);
+        output.append(start);
         for (String p : paths) {
             output.append(separator);
             output.append(p);
@@ -104,7 +144,7 @@ public class VersionControlSystem {
      * @return returns the string hash for the file
      */
     public String hash(String path) {
-        try (FileInputStream fis = new FileInputStream(path)){
+        try (FileInputStream fis = new FileInputStream(path)) {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
             byte[] dataBytes = new byte[1024];
             int bytesRead = 0;
@@ -123,18 +163,33 @@ public class VersionControlSystem {
             return null;
         }
     }
+
+    /**
+     * Checks if a file with the hash as the name is already saved
+     * @param hash: the hash
+     * @return boolean if the file is already saved.
+     */
     public boolean hashExists(String hash) {
-        return Files.exists(Paths.get(pathBuilder(new String[] {".vcs", subDirectories[0], hash.substring(0, 2), hash.substring(2)})));
+        return Files.exists(Paths.get(pathBuilder(new String[] {subDirectories[0], hash.substring(0, 2), hash.substring(2)}, this.vcsDirectory, separator)));
     }
+
+    /**
+     * Creates the bin of the first two characters of the hash if that doesn't exist, and copies the file to the bin
+     * @param path: path to the file to be copied
+     * @param hash: the hashcode of the file, for naming and bin assignment purposes
+     * @return boolean whether the creation was successful or not
+     */
     public boolean createFile(String path, String hash) {
         if (hashExists(hash)) {
             return true;
         } else {
             Path source = Paths.get(path);
-            Path target = Paths.get(pathBuilder(new String[] {".vcs", subDirectories[0], hash.substring(0, 2), hash.substring(2)}));
-            File bin = new File(pathBuilder(new String[] {".vcs", subDirectories[0], hash.substring(0, 2)}));
+            Path target = Paths.get(pathBuilder(new String[] {subDirectories[0], hash.substring(0, 2), hash.substring(2)}, vcsDirectory, separator));
+            File bin = new File(pathBuilder(new String[] {subDirectories[0], hash.substring(0, 2)}, vcsDirectory, separator));
             if (!bin.exists()) {
-                bin.mkdir();
+                if (!bin.mkdir()) {
+                    return false;
+                }
             }
             try {
                 Files.copy(source, target, StandardCopyOption.COPY_ATTRIBUTES);
