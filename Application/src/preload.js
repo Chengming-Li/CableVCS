b0eb5a32e4a928f39d42f4831211e9a0561cd67e
Chengming Li <chengming.li@berkeley.edu>
2023-04-16 01:03:08 -0700

    figured out IPC

--- Application/src/preload.js
+++ Application/src/preload.js
@@ -1,2 +1,11 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// is loaded before web content is rendered
+// contextBridge is used to securely expose APIs to the renderer, and ipcRenderer is used to send messages
+const { contextBridge, ipcRenderer } = require('electron')
+
+// exposes the window.electronAPI variable, and creating a method called setTitle(title) that sends a message
+// this method can be called with window.electronAPI.setTitle(title)
+// also creates onUpdateCounter, which opens a listening channel "update-counter"
+// Didn't expose the entire ipcRenderer.send() method for security reasons, limiting what messages we can send
+contextBridge.exposeInMainWorld('electronAPI', {
+    testDialogue: (arg) => ipcRenderer.invoke("Test", arg)
+})
--- Application/src/render.js
+++ Application/src/render.js
@@ -1,6 +1,29 @@
 const buttonOne = document.getElementById('buttonID');
-buttonOne.onclick = getVS;
 
+// adds a listener to title that'll call the code inside
+buttonOne.addEventListener('click', async () => {
+    const title = await window.electronAPI.testDialogue("HALLO")
+    console.log(title)
+});
+
+/*import { spawn } from 'child_process';
+const javaProcess = spawn('java', ['-jar', "C:\\Users\\malic\\Downloads\\Project\\VersionControlSystem\\App\\out\\artifacts\\App_jar\\App.jar"]);
+
+// Handle data/events from Java process
+javaProcess.stdout.on('data', (data) => {
+    // Handle data received from Java process
+    console.log(`Data from Java: ${data.toString()}`);
+  }
+);
+javaProcess.stderr.on('data', (data) => {
+    // Handle error data received from Java process
+    console.error(`Error from Java: ${data.toString()}`);
+});
+javaProcess.on('close', (code) => {
+    // Handle Java process exit
+    console.log(`Java process exited with code ${code}`);
+});
 async function getVS() {
-    console.log("Hello");
+    javaProcess.stdin.write('Hello from Electron!\n');
 }
+*/
