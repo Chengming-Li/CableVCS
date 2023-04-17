f199b6982c08ad5a17c60ab22f84d54bc2b291cb
Chengming Li <chengming.li@berkeley.edu>
2023-04-17 00:25:20 -0700

    added dockerIgnore

--- App/src/versioncontrolsystem/Main.java
+++ App/src/versioncontrolsystem/Main.java
@@ -2,25 +2,66 @@ package versioncontrolsystem;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-public class Main {
-    public static void main(String[] args) {
-        try {
-            System.out.println("Jar created");
-            VersionControlSystem vcs = VersionControlSystem.init("C:\\Users\\malic\\Downloads\\Test");
-            // Read input from stdin
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String input;
-            while ((input = reader.readLine()) != null) {
-                // Process input from Electron
-                System.out.println("Received input from Electron: " + input);
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-                // Send response to Electron
-                String response = vcs.getLastCommit().toString();
-                System.out.println("Sending response to Electron: " + response);
-                System.out.println(response);
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Read input from stdin
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        String[] arguments;
+        String function;
+        int first;
+        VersionControlSystem vcs = null;
+        while ((input = reader.readLine()) != null) {  // input is assigned to the message from electron
+            // Separates the input by reading the number, and separating that amount of following characters
+            first = input.charAt(0);
+            function = input.substring(1, first + 1);
+            arguments = readArgs(input.substring(first + 1));
+            // Send response to Electron
+            System.out.println(function + "\nArgs: " + Arrays.toString(arguments));
+            try {
+                if (function.equals("changeDir")) {
+                    vcs = new VersionControlSystem(arguments[0]);
+                } else if (function.equals("init")) {
+                    vcs = VersionControlSystem.init(arguments[0]);
+                } else if (vcs != null) {
+                    switch (function) {
+                        case "add" -> vcs.add(arguments[0]);
+                        case "commit" ->
+                                vcs.commit(arguments[0], arguments[1], readArgs(arguments[2]), readArgs(arguments[2]));
+                        case "remove" -> vcs.remove(arguments[0]);
+                        case "log" -> System.out.println(vcs.log());
+                        case "globalLog" -> System.out.println(vcs.globalLog());
+                        case "status" -> System.out.println(vcs.status());
+                        case "checkout" -> {
+                            if (arguments[1].startsWith("boolean")) {
+                                vcs.checkout(arguments[0], arguments[1].equals("booleanTrue"));
+                            } else {
+                                vcs.checkout(arguments[0], arguments[1]);
+                            }
+                        }
+                        case "branch" -> vcs.branch(arguments[0]);
+                        case "removeBranch" -> vcs.removeBranch(arguments[0]);
+                        case "reset" -> vcs.reset(arguments[0]);
+                    }
+                }
+            } catch (FailCaseException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
+    }
+    private static String[] readArgs(String input) {
+        List<String> output = new ArrayList<>();
+        int spot = 0;
+        int num;
+        while (spot < input.length()) {
+            num = input.charAt(spot);
+            output.add(input.substring(spot + 1, spot + num + 1));
+            spot += num + 1;
+        }
+        return output.toArray(new String[0]);
     }
 }
